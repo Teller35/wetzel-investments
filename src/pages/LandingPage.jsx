6c85261af831b022fb280db5fd77cc496df75284
Teller35 <tellerwetzel@yahoo.com>
2021-09-28 11:47:34 -0600

    got the about me section layed out

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Container, Card, Button } from "react-bootstrap";
 
 const LandingPage = () => {
-
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <section>
+      <Container className="p-3">
+        <Card bg="dark" variant="dark">
+          <Card.Header className="FontColor" as="h1">About Us</Card.Header>
+          <Card.Body>
+            <Card.Title className="FontColor" as="h2">Wetzel Investments, LLC</Card.Title>
+            <Card.Text className="FontColor" as="h4">
+              Wetzel Investments, LLC is a locally owned and operated property
+              management company. We have invested in multiple rental
+              properties. The properties that we have acquired over the years is
+              spread out throughout the Salt Lake Valley. It began as a joint
+              venture between a father and son. Now it is operated by my sister
+              Kathy Wetzel and I (Thomas Wetzel). Though the years we have
+              provided wonderful roofs over the heads a variety of families. We
+              look forward to continuing our success. If you have any further
+              inquires place feel free to reach out us.
+            </Card.Text>
+            <button className="MyButton" href="/">Contact Us</button>
+          </Card.Body>
+        </Card>
+      </Container>
+    </section>
+  );
+};
 
 export default LandingPage;
